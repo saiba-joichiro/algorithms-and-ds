d7cc0291a913a5e2c9c1af36e92abfd3be432a7d
saiba-joichiro <n3ki4@proton.me>
2023-11-30 19:15:20 +0200

    feat: implement selection sort with tests

--- algorithms.go
+++ algorithms.go
@@ -1,5 +1,7 @@
 package algorithms_and_ds
 
+import "fmt"
+
 // Binary search function
 // On success returns index of searched element
 // On fail returns -1
@@ -19,4 +21,36 @@ func BinarySearch(array []int, target int) int {
 	}
 
 	return -1
+}
+
+// Return index of min element
+func findMinElement(arr []int) int {
+	if len(arr) == 0 {
+		panic("Empty array")
+	}
+	smallest := arr[0]
+	smallestIndex := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] < smallest {
+			smallest = arr[i]
+			smallestIndex = i
+		}
+	}
+
+	return smallestIndex
+}
+
+// Selection sort algorithm
+// Return new sorted array
+func SelectionSort(arr []int) []int {
+	size := len(arr)
+	sorted := make([]int, size)
+	for i := 0; i < size; i++ {
+		smallest := findMinElement(arr)
+		fmt.Println(smallest)
+		sorted[i] = arr[smallest]
+		// TODO: Read about this. Do we modify input array because of this?
+		arr = append(arr[:smallest], arr[smallest+1:]...)
+	}
+	return sorted
 }
--- algorithms_test.go
+++ algorithms_test.go
@@ -1,6 +1,8 @@
 package algorithms_and_ds
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBinarySearchk(t *testing.T) {
 	var tests = []struct {
@@ -18,5 +20,30 @@ func TestBinarySearchk(t *testing.T) {
 		if got := BinarySearch(test.array, test.target); got != test.want {
 			t.Errorf("BinarySearch(%v,%d) = %v", test.array, test.target, got)
 		}
+	}
+}
+
+func TestSelectionSort(t *testing.T) {
+	var tests = []struct {
+		input []int
+		want  []int
+	}{
+		{[]int{4, -2, 55, 0, 4, -78}, []int{-78, -2, 0, 4, 4, 55}},
+		{[]int{}, []int{}},
+		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
+		{[]int{3, 2, 1}, []int{1, 2, 3}},
+		{[]int{32}, []int{32}},
+		{[]int{-1, -2, -55, -22}, []int{-55, -22, -2, -1}},
+	}
+
+	for _, test := range tests {
+		got := SelectionSort(test.input)
+		for i, e := range got {
+			if e != test.want[i] {
+				t.Errorf("SelectionSort(%v) = %v", test.input, test.want)
+				break
+			}
+		}
+		t.Log(test.input)
 	}
 }
